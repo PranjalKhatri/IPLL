4720e6451f7d821bcd34c4287412005f600650fb
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-01-17 23:38:34 +0530

    Updated sort logic to kth largest

--- lab2/test/test.c
+++ lab2/test/test.c
@@ -36,44 +36,40 @@ int main() {
   KeleSortFloatList();
   /*
     test input generation
+  srand(time(NULL));
   for(int i = 0;i < 100;i++){
     generate_and_find_kth();    
   }
-  printf("\n");
   */
 
+  printf("\n");
   return 0;
 }
 void generate_and_find_kth() {
   int n, k;
-
-  srand(time(NULL));
-
   n = (rand() % 16) + 5;
-
   k = (rand() % n) + 1;
 
-  printf("%d %d\n",n,k);
+  printf("%d %d\n", n, k);
   float *arr = (float *)malloc(n * sizeof(float));
   for (int i = 0; i < n; i++) {
     arr[i] = ((float)rand() / (float)(RAND_MAX)) * 100.0f;
     printf("%.2f ", arr[i]);
   }
   printf("\n");
-
   for (int i = 0; i < n - 1; i++) {
-    int max_idx = i;
+    int min_idx = i;
     for (int j = i + 1; j < n; j++) {
-      if (arr[j] > arr[max_idx]) {
-        max_idx = j;
+      if (arr[j] < arr[min_idx]) {
+        min_idx = j;
       }
     }
-    float temp = arr[max_idx];
-    arr[max_idx] = arr[i];
+    float temp = arr[min_idx];
+    arr[min_idx] = arr[i];
     arr[i] = temp;
   }
 
-  int target_idx = n - k;
+  int target_idx = n - k; 
   printf("%f\n", arr[target_idx]);
 
   free(arr);
