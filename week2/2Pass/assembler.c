24a9f3411e1e7a556080062b3d55e932ac6bc0cf
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-02-04 22:30:45 +0530

    added prints

--- week2/2Pass/assembler.c
+++ week2/2Pass/assembler.c
@@ -9,12 +9,18 @@ HashTable* SymbolTable;
 
 void Usage();
 int main(int argc, char** argv) {
-    if (argc != 3) {
+    if (argc > 3 || argc < 2) {
         Usage();
         return 1;
     }
     SymbolTable = ht_create();
-
+    int nameSz=100;
+    char outFile[nameSz];
+    if (argc == 2) {
+        snprintf(outFile, nameSz, "%s.out", argv[1]);
+    } else {
+        snprintf(outFile, nameSz, "%s", argv[2]);
+    }
     int error, retVal = 0;
     size_t numLines, programLength, firstInstruction;
     if ((error = GenerateIntermediate(argv[1], &numLines, &programLength,
@@ -22,15 +28,18 @@ int main(int argc, char** argv) {
         retVal = error;
         goto CleanupAndExit;
     }
+    printf("Pass 1 Done. Symbol Table and Intermediate outputFile generated\n");
+    printf("-------SymbolTable------\n");
     // ht_print(SymbolTable);
-    if ((error = GenerateObject(argv[1], argv[2], programLength,
+    if ((error = GenerateObject(argv[1], outFile, programLength,
                                 firstInstruction, SymbolTable))) {
         goto CleanupAndExit;
         retVal = error;
     }
+    printf("Pass 2 Done. outputFile %s Created\n", outFile);
 CleanupAndExit:
     ht_destroy(SymbolTable);
     return retVal;
 }
 
-void Usage() { printf("USAGE: assembler <inputFile> <outputFile>"); }
+void Usage() { printf("USAGE: assembler <inputFile> [outputFile]"); }
